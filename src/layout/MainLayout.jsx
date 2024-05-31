538c21ac0b29005d8a9d635b7cd0534b27ebb8b4
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-31 21:34:22 +0700

    feat: Add MainLayout component

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import Navbar from "../components/section/Navbar";
+import Footer from "../components/section/Footer";
+
+export default function MainLayout({ children }) {
+  return (
+    <div>
+      <Navbar />
+
+      <main className="pt-16">{children}</main>
+
+      <Footer />
+    </div>
+  );
+}
