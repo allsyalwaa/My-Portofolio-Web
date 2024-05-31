ece12d58bbb62782ee8796587c1f0939ec4d4863
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 00:13:22 +0700

    feat: Create CardKeyword component

---
+++ src/components/ui/CardKeyword.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+export default function CardKeyword({ text }) {
+  return (
+    <div className="rounded-full bg-primary px-4 py-2">
+      <h1 className="text-sm font-medium text-white">{text}</h1>
+    </div>
+  );
+}
