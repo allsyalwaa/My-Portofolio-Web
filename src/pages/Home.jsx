22b20ab096cba7ef0905bad21eadd8d63abf9dd1
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 00:12:13 +0700

    chore: Update Home page layout with new sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import SecAbout from "../components/section/SecAbout";
+import SecContact from "../components/section/SecContact";
+import SecHero from "../components/section/SecHero";
+import SecProjects from "../components/section/SecProjects";
+import SecSkills from "../components/section/SecSkills";
 import MainLayout from "../layout/MainLayout";
 
 export default function Home() {
-  return <MainLayout></MainLayout>;
+  return (
+    <MainLayout>
+      <SecHero />
+      <SecAbout />
+      <SecSkills />
+      <SecProjects />
+      <SecContact />
+    </MainLayout>
+  );
 }
