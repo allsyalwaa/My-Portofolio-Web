1934395f338d9ddcf21b339f8c6a6cbb7f831d3b
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 00:12:31 +0700

    chore: Update Button component with variant styles for secondary and white options

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,7 +2,7 @@
 export default function Button({ children, variant = "primary", className }) {
   return (
     <button
-      className={`flex items-center justify-center gap-4 rounded-full border-[1.5px] px-5 py-2 font-semibold ${variant === "primary" && "border-white bg-white text-sm text-primary"} ${variant === "secondary" && "border-primary bg-primary text-sm text-white"} ${variant === "white" && "border-primary bg-white text-sm text-primary"} ${className} `}
+      className={`flex items-center justify-center gap-4 rounded-full border-[1.5px] px-5 py-2 font-semibold ${variant === "primary" && "border-white bg-white text-sm text-primary"} ${variant === "secondary" && "text-md border-primary bg-primary text-white"} ${variant === "white" && "text-md border-primary bg-white text-primary"} ${className} `}
     >
       {children}
     </button>
